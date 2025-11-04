0c31b8a2d6fbe4ecf9bae14a1442672a9025b342
Timor20 <timors294@gmail.com>
2025-11-03 20:58:16 -0700

    Added docker file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ const Footer = () => {
   return (
     <footer>
       <div className="info">
-        <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
+        <p>More ways to shop: 
+          <a href="#" className="text-blue-500 underline"> Find an Apple Store</a> or 
+          <a href="#" className="text-blue-500 underline"> other retailer</a> near you. Or call 
+          <a href="#" className="text-blue-500 underline"> 1-800-MY-APPLE</a> (1-800-692-7753).
+        </p>
         <img src="/logo.svg" alt ="Apple logo"/>
       </div>
 
